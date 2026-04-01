5c4dee9c4fd7f93305b5d66a4f2d6d4abc835ea0
Marcelo Caetano <9254896+marcelo-caetano@users.noreply.github.com>
2026-04-01 13:52:19 -0700

    Updated CSS style sheet, genFooter function, and dafx2021 page Added class .figtitle to center text with larger fontsize

--- js/genFooter.js
+++ js/genFooter.js
@@ -1,5 +1,5 @@
 function genFooter() {
   var foot =
-    "This project has received funding from the European Union’s Horizon 2020 research and innovation programme under the Marie Sklodowska-Curie grant agreement No 831852 (MORPH)";
+    "This project has received funding from the European Union's Horizon 2020 research and innovation programme under the Marie Skłodowska-Curie grant agreement No 831852 (MORPH)";
   document.getElementById("footer").innerHTML = foot;
 }
